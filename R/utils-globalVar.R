ffb96576acbccd6790d7970d3d2a9d50358369da
Guillaume Boucher <guiboucher8@gmail.com>
2020-09-25 13:13:07 -0400

    Create utils-globalVar.R

---
+++ R/utils-globalVar.R
@@ -0,0 +1,22 @@
+### Just to be sure that R CMD CHECK has no NOTE messages
+
+globalVariables(sort(unique(c(
+
+  ## PACKAGES
+
+  # data.table package
+  ".", ":=",
+
+
+  # FUNCTIONS
+  # data_process
+  "by_hosp", "by_hospit",
+  "diff_grace", "drug_code", "drug_duration", "duration", "duration_ajust",
+  "grace_per",
+  "hosp",
+  "id",
+  "per",
+  "start",
+  "tx_end", "tx_end_ajust", "tx_end_grace", "tx_start"
+
+))))
