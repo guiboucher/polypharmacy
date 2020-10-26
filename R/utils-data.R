52b5f67506a6959565d3747647c63afae71d6d57
Guillaume Boucher <guiboucher8@gmail.com>
2020-10-26 08:36:59 -0400

    Create utils-data.R

---
+++ R/utils-data.R
@@ -0,0 +1,31 @@
+
+# Rx_processed ------------------------------------------------------------
+
+#' Table: Processed "unprocessed table"
+#'
+#' Table required for the calculation of the polypharmacy indicators. This table is created by using `data_process()` function on `Rx_unprocessed` data.
+#'
+#' @format A `data.table` with 6792 obs and 4 variables:
+#' \describe{
+#'   \item{id}{Individual unique identifier.}
+#'   \item{code}{Drug unique identifier.}
+#'   \item{tx_start}{The date of initiation of the reconstructed continued treatment (format as date).}
+#'   \item{tx_end}{The date of the last day of the reconstructed continued treatment (format as date).}
+#' }
+"Rx_processed"
+
+
+# Rx_unprocessed ----------------------------------------------------------
+
+#' Table: Prescription drugs deliveries
+#'
+#' Table listing all prescription drugs deliveries.
+#'
+#' @format A `data.table` with 17060 obs and 4 variables:
+#' \describe{
+#'   \item{id}{Individual unique identifier}
+#'   \item{code}{Drug unique identifier}
+#'   \item{start}{Date of the drug delivery}
+#'   \item{duration}{Duration of the delivery}
+#' }
+"Rx_unprocessed"
